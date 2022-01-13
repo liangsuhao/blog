539b1c8d0efe0b722c5974898c0f01220524396a
liangsuhao <2450381315@qq.com>
2022-01-13 20:25:32 +0800

    '写个过滤器拦截404'

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import {join} from 'path';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 const port = process.env.PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useStaticAssets(join(__dirname, '..', 'app/dist'),{
     prefix: '/',   //设置虚拟路径
   });
