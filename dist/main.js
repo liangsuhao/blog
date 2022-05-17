c939c164aadaaf4529b7ecd09a3f6775b9e476b7
liangsuhao <2450381315@qq.com>
2022-05-17 15:37:49 +0800

    增加活动模块

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const path_1 = require("path");
 const http_exception_filter_1 = require("./http-exception.filter");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3004;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
